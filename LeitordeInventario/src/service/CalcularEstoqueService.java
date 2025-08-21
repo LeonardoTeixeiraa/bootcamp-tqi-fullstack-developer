66827f0bacea8bd852fbd9e1b73639754c3b79b7
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2025-08-21 11:21:53 -0300

    feat: Implementa um programa que lê uma base de dados de produtos em .csv e faz operações de gestão de estoque e manipulação de dados

---
+++ LeitordeInventario/src/service/CalcularEstoqueService.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import com.empresa.model.Produto;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author leonardo-teixeira
+ */
+public class CalcularEstoqueService {
+
+    public double calculaTotalEstoque(List<Produto> produtos) {
+        double soma = 0;
+        
+          //Verifica se a lista de produtos esta vazia e retorna um valor double padrão
+        if (produtos == null || produtos.isEmpty()) {
+            System.out.println("A lista de produtos esta vazia");
+            return 0.0;
+        }
+        for (Produto p : produtos) {
+            soma += p.getPreco();
+        }
+        return soma;
+    }
+
+    public double calculaPrecoMedio(List<Produto> produtos) {
+        //Inicializa as variáveis
+        double mediaProdutos = 0.0;
+        double somaPrecos = 0.0;
+        int somaQuantProd = 0;
+
+            //Verifica se a lista de produtos esta vazia e retorna um valor double padrão
+        if (produtos == null || produtos.isEmpty()) {
+            System.out.println("A lista de produtos esta vazia");
+            return 0.0;
+        }
+        
+        //Percorre todos os produtos realizar a soma de todos os precos e quantidades
+        for (Produto p : produtos) {
+            somaPrecos += p.getPreco();
+            somaQuantProd += p.getQuantidadeEmEstoque();
+        }
+        //Calcula o preço medio
+        mediaProdutos = somaPrecos / somaQuantProd;
+
+        return mediaProdutos;
+    }
+    
+// Funçao que retornar o produto com o menor preço
+    public Produto calculaMaiorPreco(List<Produto> produtos) {
+        Produto produto;
+
+        //Inicializa o objeto com o preço do primeiro produto caso não estiver vazia
+        if (!produtos.isEmpty()) {
+            produto = produtos.get(0);
+        } else {
+            throw new IllegalArgumentException("A lista de produtos não pode estar vazia");
+        }
+
+        //percorre a lista de produtos atribuindo o maior preço ao objeto produto
+        for (Produto p : produtos) {
+            if (p.getPreco() > produto.getPreco()) {
+                produto = p;
+            }
+        }
+        return produto;
+    }
+
+//    Funçao que retornar o produto com o maior preço
+    public Produto calculaMenorPreco(List<Produto> produtos) {
+        Produto produto;
+
+        //Inicializa a variável com o preço do primeiro produto caso não estiver vazia
+        if (!produtos.isEmpty()) {
+            produto = produtos.get(0);
+        } else {
+            throw new IllegalArgumentException("A lista de produtos não pode estar vazia");
+        }
+
+        //Percorre a lista de produtos atribuindo o menor preco entre a variavel menorPreco
+        for (Produto p : produtos) {
+            if (p.getPreco() < produto.getPreco()) {
+                produto = p;
+            }
+        }
+        return produto;
+    }
+}
