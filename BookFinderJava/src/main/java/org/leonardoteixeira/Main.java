8264774a8579f2b259dadd82da633ca413cb8859
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2025-11-04 12:50:12 -0300

    feat: Implementa uma aplicação de busca de livros pela API GoogleBooks

---
+++ BookFinderJava/src/main/java/org/leonardoteixeira/Main.java
@@ -0,0 +1,31 @@
+package org.leonardoteixeira;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static java.net.URLEncoder.encode;
+
+public class Main {
+        public static void main(String[] args) throws IOException, InterruptedException {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Qual livro você deseja encontrar? ");
+            var busca = sc.nextLine();
+
+            String buscaCodificada = URLEncoder.encode(busca, StandardCharsets.UTF_8);
+
+            String urlBody = "https://www.googleapis.com/books/v1/volumes?q=" + buscaCodificada;
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlBody))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body());
+        }
+}
